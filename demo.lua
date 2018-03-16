9dbaf717c201056ab4ceee43fd45d2185b00cb49
Jummit <jummit@web.de>
2018-03-16 16:21:12 +0100

    enhanced demo

--- demo.lua
+++ demo.lua
@@ -14,8 +14,6 @@ elements = {
       for key, move in pairs(self.moves) do
         if engine.keyboard[key] then
           self:moveTo(elements.tilemap, move[1], move[2])
-          --self.x = self.x + move[1]
-          --self.y = self.y + move[2]
         end
       end
       elements.tilemap.lightSource.x = self.x
@@ -27,11 +25,21 @@ elements = {
     lightSource = {x=3, y=3},
     tileset = {
       floor = {bc=colors.green,tc=colors.gray,char=" "},
+      grass = {bc=colors.green,tc=colors.lime,char="\""},
       wall = {bc=colors.gray,tc=colors.lightGray,char=" ",solid=true}
     },
     generate = function(self)
       elements.tilemap:setRectangle(1, 1, engine.width, engine.height, "floor")
-      elements.tilemap:setRectangle(4, 4, 6, 4, "wall")
+      for x = 1, #self do
+        for y = 1, #self[x] do
+          if math.random(1, 10) == 1 then
+            self:setTile(x, y, "grass")
+          end
+        end
+      end
+      for i = 1, 10 do
+        elements.tilemap:setRectangle(math.random(1, engine.width-4), math.random(1, engine.height-3), 3, 2, "wall")
+      end
     end
   })
 }
